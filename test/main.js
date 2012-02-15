7240525ea740583fe3d2b65a0bacfd72d0972981
Jake Archibald <jaffathecake@gmail.com>
2012-02-15 11:19:16 +0000

    Some API thoughts

--- test/main.js
+++ test/main.js
@@ -13,6 +13,20 @@
 				.notes("Yey", "Notes")
 			.stateBullets("Will this work?")
 				.notes("And no more")
+	/* TODOs
+		.slideHeading("This is a heading")
+		.slide()
+			.addClass( classNames )
+			.stateText( string, className ) // add a div with text
+			.stateVideo( url, classNames )
+			.stateVideoPlay( pauseAtOptional )
+			.stateVideoPlay( pauseAtOptional )
+			.stateVideoSeek( time )
+				.code( string, lang, lineStart, lineEnd )
+				.codeLines( lineStart, lineEnd ) // uses previous code
+			.stateImage( url, classNames ) // should cache the img obj
+			.stateArrow( classNames ) // should float
+	*/
 	
 	;presentation.start();
 })();
