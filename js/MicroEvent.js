cfe03e30ebb9027b2ce731ca9904be85b68f6d10
Jake Archibald <jaffathecake@gmail.com>
2012-03-08 11:07:22 -0600

    Optimising MicroEvent

--- js/MicroEvent.js
+++ js/MicroEvent.js
@@ -1,24 +1,34 @@
-function MicroEvent() {}
+(function() {
+	function MicroEvent() {}
 
-MicroEvent.prototype = {
-	on: function(event, fct){
-		this._events = this._events || {};
-		this._events[event] = this._events[event] || [];
-		this._events[event].push(fct);
-		return this;
-	},
-	off: function(event, fct){
-		this._events = this._events || {};
-		if ( !(event in this._events) ) { return this; }
-		this._events[event].splice(this._events[event].indexOf(fct), 1);
-		return this;
-	},
-	trigger: function(event /* , args... */){
-		this._events = this._events || {};
-		if ( !(event in this._events) ) { return this; }
-		for (var i = 0, args = Array.prototype.slice.call(arguments, 1), len = this._events[event].length; i < len; i++) {
-			this._events[event][i].apply( this, args );
+	var slice = Array.prototype.slice;
+
+	MicroEvent.prototype = {
+		on: function(event, fct){
+			this._events = this._events || {};
+			this._events[event] = this._events[event] || [];
+			this._events[event].push(fct);
+			return this;
+		},
+		off: function(event, fct){
+			this._events = this._events || {};
+			if ( !(event in this._events) ) { return this; }
+			this._events[event].splice(this._events[event].indexOf(fct), 1);
+			return this;
+		},
+		trigger: function(event /* , args... */){
+			var events = this._events || {};
+
+			if ( !(event in events) ) { return this; }
+
+			var eventType = events[event];
+
+			for (var i = 0, args = slice.call(arguments, 1), len = eventType.length; i < len; i++) {
+				eventType[i].apply( this, args );
+			}
+			return this;
 		}
-		return this;
-	}
-};
+	};
+
+	window.MicroEvent = MicroEvent;
+})();
