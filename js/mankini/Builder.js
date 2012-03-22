04d39b155741085c31ddd9dcaed848e3f049c77e
Jake Archibald <jaffathecake@gmail.com>
2012-03-22 08:26:40 +0000

    Adding handwriting font

--- js/mankini/Builder.js
+++ js/mankini/Builder.js
@@ -7,7 +7,7 @@
 
 	function Builder(presentation) {
 		this._presentation = presentation;
-		this._notes = new mankini.Notes( 1 );
+		this._notes = new mankini.Notes( 0 );
 		this._pointer = new mankini.Pointer();
 		this._pointer.$container.appendTo( document.body );
 		this._$authorInfo = $('.author');
