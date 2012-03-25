b022a23dc4df2f7ce07c51b043ef3b0cff5f3a92
Jake Archibald <jaffathecake@gmail.com>
2012-03-25 15:48:54 +0100

    Quick frame capture extension

---
+++ capture-extension/script.js
@@ -0,0 +1,24 @@
+(function() {
+	var index = 1;
+	chrome.browserAction.onClicked.addListener(function(tab) {
+		chrome.tabs.captureVisibleTab(tab.windowId, {
+			format: 'png'
+		}, function(data) {
+			var canvas = document.createElement('canvas'),
+				img = new Image();
+			canvas.width = 1024;
+			canvas.height = 768;
+			img.onload = function() {
+				var filePrefix = String(index++);
+				filePrefix = Array( 4 - filePrefix.length ).join('0') + filePrefix;
+				canvas.getContext('2d').drawImage(img, 0, 0);
+				var a = document.createElement('a');
+				a.href = canvas.toDataURL();
+				a.download = filePrefix + '.png';
+				a.click();
+			};
+			img.src = data;
+
+		});
+	});
+})();
